f756c34780cdac5ef0624be991c8cd55d41de944
Bilbobx182 <c14474048@mydit.ie>
2017-05-28 17:59:12 +0100

    Doggo comments

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -10,4 +10,9 @@ export class EntryComponent {
   title = 'His name is Cosmo';
   photo = 'http://i.imgur.com/ZuUrzGW.jpg';
   description = 'My little doggo';
+
+  comments: any[] = [
+    {name: 'Mark', comment: ' Ciaran your dog looks dope'},
+    {name: 'Bill', comment: ' Good doggo'},
+  ];
 }
